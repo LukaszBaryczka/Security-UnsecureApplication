48dfc02d9ebce789b52780445013ddabc83af9bf
Lukasz Baryczka <l.baryczka@gmail.com>
2018-05-24 18:25:38 +0200

    init project

---
+++ src/main/java/ftims/security/unsecure/app/unsecureApp/UnsecureAppApplication.java
@@ -0,0 +1,12 @@
+package ftims.security.unsecure.app.unsecureApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsecureAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnsecureAppApplication.class, args);
+	}
+}
---
+++ src/test/java/ftims/security/unsecure/app/unsecureApp/UnsecureAppApplicationTests.java
@@ -0,0 +1,16 @@
+package ftims.security.unsecure.app.unsecureApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UnsecureAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
